a83c4ebc987e53651319e5813726c512c843b704
Per Thykjaer Jensen <per@multimusen.dk>
2023-10-11 07:18:48 +0200

    first commit

---
+++ ecom-REST/oplysninger.js
@@ -0,0 +1,13 @@
+// kundenøgle
+custKey = "ck_1b7c550d193eceaece991fdff96c8c1280cc43c0"
+
+// kundekodeord
+custSecret = "cs_a642dfe44f6455a67f167443604beb8e480f6a57"
+
+// URL til products
+// https://woo.thoth.dk//wp-json/wc/v3/products?consumer_key=ck_1b7c550d193eceaece991fdff96c8c1280cc43c0&consumer_secret=cs_a642dfe44f6455a67f167443604beb8e480f6a57
+// :-)
+
+
+
+
