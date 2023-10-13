39fb2482e2e46a26eb31cca8d22001c68033b802
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2023-10-13 13:04:26 +0200

    key and secret removed
    and yeah, they key pair has been deleted on the server ;-)

--- woo/js/restapi.js
+++ woo/js/restapi.js
@@ -5,15 +5,16 @@ console.log("Ok, from ./js/restapi.js")
 const endPoint = "https://woo.thoth.dk/wp-json/wc/v3/products"
 
 // consumer key:
-const ck =  "consumer_key=" 
-            + "ck_1b7c550d193eceaece991fdff96c8c1280cc43c0"
+let ck  =  "consumer_key=" 
+ck      += "CUSUMER-KEY-HERE"
 
 // consumer secret:
-const cs =  "consumer_secret=" 
-            + "cs_a642dfe44f6455a67f167443604beb8e480f6a57"
+let cs  =  "consumer_secret=" 
+cs      += "CONSUMER-SECRETE-HERE"
 
 // combine key and secret with the endpoint:
 const wooRest = endPoint + "?" + ck + "&" + cs
+
 // add more key value pairs if you need it
 
 // now fetch() the data from the const woorest
@@ -30,27 +31,29 @@ fetch( wooRest ).then(
 
         // Sample data (uncomment and watch this in the Inspect Tool / Console):
         // ....
-        //console.log(data[0].images[0].name) // name of the image
-        //console.log(data[0].images[0].src) // src of the image
+        // console.log(data[3].images[0].name) // name of the image
+        // console.log(data[0].images[0].src) // src of the image
         // ....
 
         // loop through the data object
+        
         for (i=0; i < data.length; i++ ){
 
             // this will add the procucts as HTML to <div id="katalog"></div>
-            katalog.innerHTML += `
+            catalog.innerHTML += `
                 <section class="produkter">
                     <h3> ${data[i].name} </h3>
                     <figure>
                         <img 
-                            src="${data[i].images[0].src}" 
+                            src="${ data[i].images[0].src }" 
                             alt="${data[i].images[0].name}"
                             class="productImg">
                         <figcaption> ${data[i].description} </figcaption>
-                        <a href="${data[i].permalink}"> Bestil nu! </a>
+                        <a href="${data[i].permalink}"> Order Now! </a>
                     </figure>
                 </section>`
         } 
+        
 
     }).catch(err => {
 
